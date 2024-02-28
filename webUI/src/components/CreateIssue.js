0e3065e0bdd2a4fb321b37ac45142721bac4b32a
Alan K Mathew <sill_mileage_04@icloud.com>
2024-02-28 18:22:07 +0100

    Refactor app layout and add sidebar

---
+++ webUI/src/components/CreateIssue.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreateIssue = () => {
+  return <div>Issue Content</div>;
+};
+
+export default CreateIssue;
---
+++ webUI/src/components/Sidebar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../Assets/logo.png';
+import userProfile from '../Assets/user-profile.png';
+import logoutIcon from '../Assets/logout-icon.png';
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <img src={logo} alt="Logo" className="sidebar-logo" />
+      <div className="sidebar-footer">
+        <img src={logoutIcon} alt="Logout" className="sidebar-icon" />
+        <img src={userProfile} alt="User" className="sidebar-icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
