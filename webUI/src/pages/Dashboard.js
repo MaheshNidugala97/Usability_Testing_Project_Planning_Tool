2a75be927cfe20c32ed0febcc53c9eb9683f6be3
Alan K Mathew <sill_mileage_04@icloud.com>
2024-02-29 14:10:09 +0100

    Refactor App component and add Dashboard page

---
+++ webUI/src/pages/Dashboard.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/NavBar';
+import Backlog from '../components/Backlog';
+import Board from '../components/Board';
+import ProjectSettings from '../components/ProjectSettings';
+import CreateIssue from '../components/CreateIssue';
+import '../styles/Dashboard.css';
+
+
+function App() {
+  const [activeSection, setActiveSection] = useState('Board');
+
+  const getActiveComponent = () => {
+    switch (activeSection) {
+      case 'Board': return <Board />;
+      case 'Project Settings': return <ProjectSettings />;
+      case 'Create Issue': return <CreateIssue />;
+      case 'Backlog': return <Backlog />;
+      default: return <div>Select a project from the menu</div>;
+    }
+  };
+
+  return (
+    <div className="app">
+      <Sidebar />
+      <div className="main-section">
+        <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+        <div className="main-container">
+          {getActiveComponent()}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
