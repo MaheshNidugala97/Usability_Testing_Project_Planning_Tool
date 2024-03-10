3f32084db2555b01f95860febe23735b5f49b3a1
Alan K Mathew <sill_mileage_04@icloud.com>
2024-03-10 21:27:04 +0100

    update-CreateIssue

--- webUI/src/components/CreateIssue.jsx
+++ webUI/src/components/CreateIssue.jsx
@@ -1,54 +1,60 @@
-import React, { useState } from "react";
-import axios from "axios";
-import "../styles/CreateIssue.css";
+import React, { useState } from 'react';
+import axios from 'axios';
+import '../styles/CreateIssue.css';
 
 const CreateIssue = ({ onClose }) => {
   const initialState = {
-    title: "",
-    description: "",
-    status: "To Do",
-    priority: "LOW",
-    assignee: "Daniel",
-    reporter: "Daniel",
+    title: '',
+    description: '',
+    status: 'Backlog',
+    priority: 'LOW',
+    assignee: 'Daniel',
+    reporter: 'Daniel',
+    estimate: 0 
   };
 
   const [issue, setIssue] = useState(initialState);
+  const [estimate, setEstimate] = useState(0); 
 
-  const assignees = ["Mark", "John", "Daniel"];
-  const reporters = ["Mark", "John", "Daniel"];
+  const assignees = ['Mark', 'John', 'Daniel'];
+  const reporters = ['Mark', 'John', 'Daniel'];
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setIssue({ ...issue, [name]: value });
   };
 
+  const handleEstimateChange = (e) => {
+    const value = parseInt(e.target.value);
+    setEstimate(value);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const id = Math.floor(Math.random() * 10000000);
-      const issueWithId = { ...issue, id };
-
-      console.log("Submitting issue:", issueWithId);
-
-      const response = await axios.post(
-        "http://localhost:3009/api/issues",
-        issueWithId
-      );
-
+      const time = new Date().toISOString();
+      const ticketNumber = Math.floor(Math.random() * 1000);
+      const ticketName = `T-${ticketNumber}`;
+      const issueWithId = { ...issue, id, estimate, time, ticketName };
+  
+      console.log('Submitting issue:', issueWithId);
+  
+      const response = await axios.post('http://localhost:3009/api/issues', issueWithId);
+      
       alert(response.data.message);
       setIssue(initialState);
+      setEstimate(0); 
       onClose();
     } catch (error) {
-      console.error("There was an error creating the issue:", error);
+      console.error('There was an error creating the issue:', error);
     }
   };
 
   return (
     <div className="modal-overlay">
       <form onSubmit={handleSubmit} className="issue-form">
-        <button type="button" className="close-button" onClick={onClose}>
-          ×
-        </button>
+        <button type="button" className="close-button" onClick={onClose}>×</button>
         <h2>Create new issue</h2>
         <div className="form-row">
           <div className="form-column">
@@ -60,6 +66,16 @@ const CreateIssue = ({ onClose }) => {
               value={issue.title}
               onChange={handleInputChange}
               placeholder="Write the title"
+              required
+            />
+             <label htmlFor="estimate">Estimate</label>
+            <input
+              id="estimate"
+              name="estimate"
+              type="number"
+              value={estimate}
+              onChange={handleEstimateChange}
+              placeholder="Enter estimate"
               required
             />
             <label htmlFor="description">Description</label>
@@ -74,58 +90,32 @@ const CreateIssue = ({ onClose }) => {
           </div>
           <div className="form-column">
             <label htmlFor="status">Status</label>
-            <select
-              id="status"
-              name="status"
-              value={issue.status}
-              onChange={handleInputChange}
-            >
-              <option value="To Do">To do</option>
-              <option value="In Progress">In Progress</option>
-              <option value="Done">Done</option>
+            <select id="status" name="status" value={issue.status} onChange={handleInputChange}>
+              <option value="To Do">To Do</option>
+              <option value="Backlog">Backlog</option>
+             
             </select>
             <label htmlFor="priority">Priority</label>
-            <select
-              id="priority"
-              name="priority"
-              value={issue.priority}
-              onChange={handleInputChange}
-            >
+            <select id="priority" name="priority" value={issue.priority} onChange={handleInputChange}>
               <option value="LOW">LOW</option>
               <option value="MEDIUM">MEDIUM</option>
               <option value="HIGH">HIGH</option>
             </select>
             <label htmlFor="assignee">Assignee</label>
-            <select
-              id="assignee"
-              name="assignee"
-              value={issue.assignee}
-              onChange={handleInputChange}
-            >
-              {assignees.map((assignee) => (
-                <option key={assignee} value={assignee}>
-                  {assignee}
-                </option>
+            <select id="assignee" name="assignee" value={issue.assignee} onChange={handleInputChange}>
+              {assignees.map(assignee => (
+                <option key={assignee} value={assignee}>{assignee}</option>
               ))}
             </select>
             <label htmlFor="reporter">Reporter</label>
-            <select
-              id="reporter"
-              name="reporter"
-              value={issue.reporter}
-              onChange={handleInputChange}
-            >
-              {reporters.map((reporter) => (
-                <option key={reporter} value={reporter}>
-                  {reporter}
-                </option>
+            <select id="reporter" name="reporter" value={issue.reporter} onChange={handleInputChange}>
+              {reporters.map(reporter => (
+                <option key={reporter} value={reporter}>{reporter}</option>
               ))}
             </select>
           </div>
         </div>
-        <button type="submit" className="accept-button">
-          Accept
-        </button>
+        <button type="submit" className="accept-button">Accept</button>
       </form>
     </div>
   );
