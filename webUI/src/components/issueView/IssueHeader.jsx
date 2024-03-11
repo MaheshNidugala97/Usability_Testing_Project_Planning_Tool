31a3f7a3706ccfe5593cfaa05ebb467d81dc24ba
Alan K Mathew <sill_mileage_04@icloud.com>
2024-03-11 09:40:48 +0100

    Added - ticket creation date

--- webUI/src/components/issueView/IssueHeader.jsx
+++ webUI/src/components/issueView/IssueHeader.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import '../../styles/issueView/IssueHeader.css';
 
-const IssueHeader = ({ onClose, onExpand, isExpanded }) => (
-  <div className={`header-issue ${isExpanded ? 'expanded' : ''}`}>
-    <button type="button" className="issue-close-button" onClick={onClose}>
-      ×
-    </button>
-    <button type="button" className="expand-button" onClick={onExpand}>
-      {isExpanded ? "Collapse" : "Expand"}
-    </button>
-  </div>
-);
+const IssueHeader = ({ onClose, onExpand, isExpanded, time }) => {
+  const date = new Date(time);
+
+  const formattedDate = date.toLocaleDateString('en-US', {
+    weekday: 'long', 
+    year: 'numeric', 
+    month: 'long', 
+    day: 'numeric'
+  });
+
+  return (
+    <div className={`header-issue ${isExpanded ? 'expanded' : ''}`}>
+      <p className="issue-date">{formattedDate}</p>
+      <div>
+        <button type="button" className="expand-button" onClick={onExpand}>
+          {isExpanded ? "Collapse" : "Expand"}
+        </button>
+        <button type="button" className="issue-close-button" onClick={onClose}>
+          ×
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default IssueHeader;
